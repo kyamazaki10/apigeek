1f6e39dc242b88f3c44eb5bbc463e2fae2e6846c
Kumiko Yamazaki <kyamazaki@gmail.com>
2015-09-25 22:38:16 -0600

    adding optional callback function to prettify the JSON reponse

--- public/scripts/analytics.js
+++ public/scripts/analytics.js
@@ -13,7 +13,50 @@ A.Analytics = {
             'filter' : '(developer_email eq \'' + params.email + '\')'
         };
 
-        A.Utils.submitRequest(this.url, data);
+        A.Utils.submitRequest(this.url, data, this.prettifyTransactions);
+    },
+
+    /**
+     * Prettifying the transactions into a table
+     */
+    prettifyTransactions: function(data) {
+        var proxies = data.environments[0].dimensions;
+        var thead = '';
+        var tbody = '';
+        var date = [];
+        var values;
+
+        // loop through each proxy
+        for (var i=0; i<proxies.length; i++) {
+            // store the proxy name
+            thead += '<th>' + proxies[i].name + '</th>';
+
+            // the values are a single array of all the dates and the transaction
+            // count (for that single proxy) for that specific date
+            values = proxies[i].metrics[0].values;
+
+            // each date gets its own array for easy generation of html markup
+            for (var j=0; j<values.length; j++) {
+                date[j] = date[j] || [];
+
+                // push the proxy's transaction count into each date's array
+                date[j].push([values[j].value]);
+            }
+        }
+
+        // loop through the each date's array
+        for (var k=0; k<date.length; k++) {
+            tbody += '<tr>';
+
+            for (var l=0; l<date[k].length; l++) {
+                tbody += '<td>' + date[k][l] + '</td>';
+            }
+
+            tbody += '</tr>';
+        }
+
+        $('thead').html(thead);
+        $('tbody').html(tbody);
     }
 
 }
--- public/scripts/utils.js
+++ public/scripts/utils.js
@@ -3,7 +3,7 @@ A.Utils = {
     /**
      * Submit AJAX request
      */
-    submitRequest: function(url, data) {
+    submitRequest: function(url, data, callback) {
         var auth = btoa(A.Config.user + ':' + A.Config.pw);
 
         $.ajax({
@@ -16,9 +16,15 @@ A.Utils = {
 
             // show JSON response
             success: function(data) {
+                // callback function to prettify the JSON response
+                if (callback) {
+                    callback(data);
+                }
+
+                // show raw JSON response
                 $('.result-json').html(JSON.stringify(data, null, 2));
             }
-        })
+        });
     }
 
 }
